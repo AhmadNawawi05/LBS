5aa7d85480d40b8984376a55ab68786b11ffd506
AhmadNawawwiUmar <21221753095@smkassalaambandung>
2022-11-26 00:11:07 +0700

    UPDATE 1

---
+++ X/Quiz/matematika/soal1.js
@@ -0,0 +1,71 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Apa itu Matriks",
+    answer: "Susunan Bilangan",
+    options: [
+      "Alat Hitung",
+      "Susunan Bangun Ruang",
+      "Susunan Bilangan",
+      "Alat Jual Beli"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Ordo adalah?",
+    answer: "Elemen baris dan Elemen kolom",
+    options: [
+      "Elemen baris dan Elemen kolom",
+      "Susunan Bangun Ruang",
+      "Sebuah reformasi",
+      "Sebuah operator"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Apa fungsi dari matriks?",
+    answer: "Untuk menyelesaikan berbagai permasalahan matematika",
+    options: [
+      "Untuk menyelesaikan berbagai permasalahan matematika",
+      "Menghitung bangun ruang",
+      "Menyelesaikan permasalahan kompleks",
+      "Menghitung susunan angka"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Ada berapa jenis matriks?",
+    answer: "10",
+    options: [
+      "10",
+      "14",
+      "7",
+      "3"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Tranpose matriks adalah?",
+    answer: "Matriks baru yang diperoleh dengan cara menukar",
+    options: [
+      "Matriks baru yang diperoleh dengan cara menukar",
+      "Perkalian Matriks",
+      "Persamaan Matriks",
+      "Matriks Penyempurnaan"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
---
+++ XII/Quiz/matematika/soal1.js
@@ -0,0 +1,71 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Apa itu Matriks",
+    answer: "Susunan Bilangan",
+    options: [
+      "Alat Hitung",
+      "Susunan Bangun Ruang",
+      "Susunan Bilangan",
+      "Alat Jual Beli"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Ordo adalah?",
+    answer: "Elemen baris dan Elemen kolom",
+    options: [
+      "Elemen baris dan Elemen kolom",
+      "Susunan Bangun Ruang",
+      "Sebuah reformasi",
+      "Sebuah operator"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Apa fungsi dari matriks?",
+    answer: "Untuk menyelesaikan berbagai permasalahan matematika",
+    options: [
+      "Untuk menyelesaikan berbagai permasalahan matematika",
+      "Menghitung bangun ruang",
+      "Menyelesaikan permasalahan kompleks",
+      "Menghitung susunan angka"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand ?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
